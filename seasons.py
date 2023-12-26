74be7a613f34d51126995db952031a3d4edc2493
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-26 01:11:36 -0800

    seasons last commit

--- seasons.py
+++ seasons.py
@@ -1,33 +1,38 @@
 """
 In a file called seasons.py, implement a program that prompts the user for their date of birth in YYYY-MM-DD format and then sings prints how old they are in minutes, rounded to the nearest integer, using English words instead of numerals, just like the song from Rent, without any and between words. Since a user might not know the time at which they were born, assume, for simplicity, that the user was born at midnight (i.e., 00:00:00) on that date. And assume that the current time is also midnight. In other words, even if the user runs the program at noon, assume that it’s actually midnight, on the same date. Use datetime.date.today to get today’s date, per docs.python.org/3/library/datetime.html#datetime.date.today.
 """
-
-import re
 import sys
-import inflect
 from datetime import date
+import re
+import inflect
 
-p = inflect.engine()
+
 
 def main():
-    user_birth_day = input('When is your birthday? ')
+    p = inflect.engine()
     try:
-      year,month,day = check_date(user_birth_day)
-    except ValueError:
+        user_birthday = input('Enter your birthdate: ')
+        year,month,day = check_date(user_birthday)
+    except (ValueError,TypeError):
         sys.exit('Invalid input')
-      
-    valid_birth_date = date(int(year),int(month),int(day))
-    date_of_today = date.today()    
-    diff = date_of_today - valid_birth_date
-    diff_minutes = diff.days * 24 * 60
+    valid_date_birth = date(year,month,day)
+    date_of_today = date.today()
+    date_diff = date_of_today - valid_date_birth
+    diff_min = date_diff.days* 24 * 60
+    date_diff_word = p.number_to_words(diff_min , andword="")
+    print(date_diff_word)
 
-    output = p.number_to_words(diff_minutes, andword="")
-    print(output)
-    
-def check_date(user_birth_day):
-    if birth_date := re.search(r'^([0-9]{4})-([0-9]{2})-([0-9]{2})$', user_birth_day ) :
-        year,month,day = birth_date.groups()
-        return year,month,day
+
+def check_date(user_birthday):
+   if checked_birthday := re.search('^([0-9]{4})-([0-9]+)-([0-9]+)$',user_birthday):
+      year,month,day = checked_birthday.groups()
+      if  int(month) <= 12 and int(day) <= 31 and  int(year) <=2025 :
+         return int(year), int(month) , int(day) 
+ 
+
+
+
+
 
 if __name__ == "__main__":
     main()  
